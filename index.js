571976c59006951b25d59922c4dec451d3017f46
Rylan Wassem <dryland28@gmail.com>
2020-09-24 15:22:47 -0400

    finished implementing CRUD

--- index.js
+++ index.js
@@ -26,62 +26,72 @@ function renderPage() {
 
 }
 
-const URL = `https://thinkful-list-api.herokuapp.com/rylan/`;
+const URL = 'https://thinkful-list-api.herokuapp.com/rylan/';
 
 function send(event, data) {
 
 }
 
-// This functions gets all of the bookmarks from the server.
-function getBookmarks() {
-  fetch(`${URL}bookmarks`)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((e) => {
-      console.log('Request failed', e);
-    });
-}
+// This function CREATES a bookmark and writes it to the data model.
+function createBookmark() {
 
-// This function creates a bookmark and writes it to the data model.
-function postBookmark() {
-  
-  let data = { "title": "Google", "url": "http://google.com", "desc":"a description", "rating":2 };
+  let data = { 'title': 'Google', 'url': 'http://google.com', 'desc': 'a description', 'rating': 2 };
 
   let options = {
     method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json'
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
     body: JSON.stringify(data)
   };
 
   fetch(`${URL}bookmarks`, options)
     .then(response => response.json())
-    .catch(e => {
-      console.log('Request failed', e);
+    .catch(error => {
+      console.log('Request failed', error);
     });
 }
 
-//This function deletes a bookmark with a given id.
-function deleteBookmark (id) {
-  
+// This functions READS all of the bookmarks from the server.
+function readBookmarks() {
+  fetch(`${URL}bookmarks`)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((error) => {
+      console.log('Request failed', error);
+    });
+}
+
+// This function UPDATES a bookmark with the provided id and data.
+// The data argument is an object consisting of a key/value pair.
+// example data: { "rating":3 }
+function updateBookmark(id, data) {
+
   let options = {
-    method: 'DELETE',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer'
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
   };
 
   fetch(`${URL}bookmarks/${id}`, options)
-    .then((r) => r.json())
-    .catch((e) => {
-      console.log('Request failed', e);
+    .then(response => response.json())
+    .catch(error => {
+      console.log('Request failed', error);
+    });
+}
+
+// This function DELETES a bookmark with a given id from the data set.
+function deleteBookmark(id) {
+
+  let options = {
+    method: 'DELETE'
+  };
+
+  fetch(`${URL}bookmarks/${id}`, options)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log('Request failed', error);
     });
 }
