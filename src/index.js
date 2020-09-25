d4b1d929d6f1035e9052f0b0c31bdc7f784bc7d8
Rylan Wassem <dryland28@gmail.com>
2020-09-25 11:40:18 -0400

    ui design

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import $ from 'jquery';
 import './index.css';
 
 function generatePage() {
-  return `
-    <div>
+  return`
+  <div>
       <div>
-          <h1>Bookmarks:</h1>
+        <h1>Bookmarks:</h1>
       </div>
-      <div><button>New</button>
+      <div>
+          <button>New</button>
           <select>
               <option>Filter By</option>
               <option>1 star</option>
@@ -15,41 +16,59 @@ function generatePage() {
               <option>3 stars</option>
               <option>4 stars</option>
               <option>5 stars</option>
-          </select></div>
-      <div>
-          <div>
-              <div>Google</div>
-              <div></div>
-              <div>
-                  <a href="https://www.google.com">Visit Site</a>
-                  <textarea>google</textarea>
-              </div>
-              <div>
-                  <input type="radio" name="rating" value="1">
-                  <input type="radio" name="rating" value="2">
-                  <input type="radio" name="rating" value="3">
-                  <input type="radio" name="rating" value="4">
-                  <input type="radio" name="rating" value="5">
-              </div>
-          </div>
-          <div>
-              <div>NetFlix</div>
-              <div></div>
-              <div>
-                  <a href="https://www.netflix.com">Visit Site</a>
-                  <textarea class="">netflix</textarea>
-              </div>
-              <div>
-                  <input type="radio" name="rating" value="1">
-                  <input type="radio" name="rating" value="2">
-                  <input type="radio" name="rating" value="3">
-                  <input type="radio" name="rating" value="4">
-                  <input type="radio" name="rating" value="5">
-              </div>
-          </div>
+          </select>
       </div>
+  <div>
+  <ul id="accordion">
+        <li>
+            <label for="first">Google 3 stars</label>
+            <input type="radio" name="accordion" id="first">
+            <div class="content">
+              <p>
+                    <a href="https://www.google.com">Visit Site</a>
+                    <textarea>Google</textarea>
+                    <input type="radio" name="rating" value="1">
+                    <input type="radio" name="rating" value="2">
+                    <input type="radio" name="rating" value="3">
+                    <input type="radio" name="rating" value="4">
+                    <input type="radio" name="rating" value="5">
+              </p>
+            </div>
+        </li>
+        <li>
+            <label for="second">Google 3 stars</label>
+            <input type="radio" name="accordion" id="second">
+            <div class="content">
+              <p>
+                    <a href="https://www.google.com">Visit Site</a>
+                    <textarea>Google</textarea>
+                    <input type="radio" name="rating" value="1">
+                    <input type="radio" name="rating" value="2">
+                    <input type="radio" name="rating" value="3">
+                    <input type="radio" name="rating" value="4">
+                    <input type="radio" name="rating" value="5">
+              </p>
+            </div>
+        </li>
+        <li>
+            <label for="third">Google 3 stars</label>
+            <input type="radio" name="accordion" id="third">
+            <div class="content">
+              <p>
+                    <a href="https://www.google.com">Visit Site</a>
+                    <textarea>Google</textarea>
+                    <input type="radio" name="rating" value="1">
+                    <input type="radio" name="rating" value="2">
+                    <input type="radio" name="rating" value="3">
+                    <input type="radio" name="rating" value="4">
+                    <input type="radio" name="rating" value="5">
+              </p>
+            </div>
+        </li>
+    </ul>
     </div>
-    `;
+    </div>
+  `;
 }
   
 function renderPage() {
